1ce8ebd01b9fd49621297c4bd1875c56a82de720
Luis Gaudencio <luis_g24@live.com>
2023-06-06 21:09:41 +0000

    Define variables using javascript for the modal

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+let modal = document.querySelector("#modal");
+
+let openButton = document.querySelector("#openModal"); 
+
+let closeButton = document.querySelector(".close-modal")[0];
+
+let okButton = document.querySelector(".footer-button")[0]; 
