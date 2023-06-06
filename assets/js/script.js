33d0e9b377dbb78964af5464a1c3a92c4d98323d
Luis Gaudencio <luis_g24@live.com>
2023-06-06 21:15:49 +0000

    Add onclick events for the modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,9 @@ let openButton = document.querySelector("#openModal");
 let closeButton = document.querySelector(".close-modal")[0];
 
 let okButton = document.querySelector(".footer-button")[0]; 
+
+openButton.onclick = () => modal.style.display = "block";
+
+closeButton.onclick = () => modal.style.display = "none";
+
+okButton.onclick = () => modal.style.display = "none"; 
