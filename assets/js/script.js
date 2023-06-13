e5190290e66323c1944d83f3b0ac7688b243e153
Luis Gaudencio <luis_g24@live.com>
2023-06-13 21:04:00 +0000

    Delete modal javascript from script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,3 @@
-// JAVASCRIPT FOR THE MODAL 
-
-// Declare variable for the modal
-let modal = document.querySelector("#modal");
-
-// Declare variable that will open the modal
-let openButton = document.querySelector("#openModal"); 
-
-// Declare variable for the <span> element which will close the modal
-let closeButton = document.getElementsByClassName("close-modal")[0];
-
-// Declare variable for the 'Ok' button which will also close the modal
-let okButton = document.getElementsByClassName("footer-button")[0]; 
-
-// The modal will open when the 'Before Beginning, Click me for Rules and Instructions' button is clicked
-openButton.onclick = () => modal.style.display = "block";
-
-// When <span> (x) is clicked, the modal will close
-closeButton.onclick = () => modal.style.display = "none";
-
-// When the 'Ok' button is clicked, the modal will also close
-okButton.onclick = () => modal.style.display = "none"; 
-
-
 // JAVASCRIPT FOR THE QUIZ
 
 // Declare varibale so the questions on the JSON file will be displayed to the user
