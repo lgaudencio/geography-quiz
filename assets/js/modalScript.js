a34fdd75d6647b22ef93fd59022e83458b6ac46a
Luis Gaudencio <luis_g24@live.com>
2023-07-07 21:09:04 +0000

    Add modal credits to the modalScript.js file

--- assets/js/modalScript.js
+++ assets/js/modalScript.js
@@ -1,5 +1,13 @@
 // JAVASCRIPT FOR THE MODAL 
 
+/* The JavaScript code for the modal was based on the one 
+    found on the W3Schools website and can be found at: 
+    https://www.w3schools.com/howto/howto_css_modals.asp
+
+    For best practice, ES6 was used by the use of 
+    arrow functions.
+*/
+
 // Declare variable for the modal
 let modal = document.querySelector("#modal");
 
